7e93225fc5622f22bab9fe485c6c48a115bcc5dc
Giovane Vicente <giponvi@gmail.com>
2026-08-07 01:52:07 -0300

    exercicio 21 finalizado

---
+++ 021.py
@@ -0,0 +1,45 @@
+from rich import print
+from rich.traceback import install
+install()
+
+class Caneta:
+    def __init__(self, cor="azul"):
+        match cor.lower().strip():
+            case "azul":
+                escolha="[blue]"
+            case "vermelho"|"vermelha":
+                escolha="[red]"
+            case "verde":
+                escolha="[green]"
+            case _:
+                escolha="[white]"
+        self.cor = escolha
+        self.tampado = True
+
+    def destampar(self):
+        if self.tampado:
+            self.tampado=False
+
+    def escrever(self, conteudo):
+        if self.tampado:
+            print('Não pode escrever, destampe a caneta!')
+        else:
+            print(f"{self.cor}{conteudo}[/]")
+
+    def quebrar_linhas(self, quantidade):
+        for i in range(quantidade):
+            print()
+
+c1=Caneta("red")
+c2=Caneta("Blue")
+c3=Caneta("Green")
+
+c1.destampar()
+c2.destampar()
+c3.destampar()
+
+c1.escrever("HAHA")
+c1.quebrar_linhas(1)
+c2.escrever("HAHA")
+c2.quebrar_linhas(2)
+c3.escrever("HAHA")
